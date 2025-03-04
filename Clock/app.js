bad24057087c7685f6c98bbe064a288378f84635
Avanish rai <168912943+avanishrai75@users.noreply.github.com>
2025-03-04 20:17:58 +0530

    Add files via upload

---
+++ Clock/app.js
@@ -0,0 +1,17 @@
+setInterval(()=>{
+    let time = new Date ();
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
+
+
+    let hRotation = 30*hours+minutes/2;
+    let mRotation = 6*minutes;
+    let sRotation =6*seconds;
+
+
+document.getElementById("hour").style.transform=`rotate(${hRotation}deg)`;
+document.getElementById("minute").style.transform=`rotate(${mRotation}deg)`;
+document.getElementById("second").style.transform=`rotate(${sRotation}deg)`;
+
+},1000)
